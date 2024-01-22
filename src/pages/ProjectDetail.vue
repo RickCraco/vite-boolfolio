fce573d864a2687337d16b7def395fd9544ed3ae
RickCraco <riccardocracolici43@gmail.com>
2024-01-22 16:46:02 +0100

    added style to project detail component

--- src/pages/ProjectDetail.vue
+++ src/pages/ProjectDetail.vue
@@ -1,6 +1,21 @@
 <template>
-    <h2>ProjectDetail</h2>
-    <h3>{{ this.project.title }}</h3>
+    <div class="container mt-5">
+        <h2>ProjectDetail</h2>
+        <h3 class="mt-3">{{ this.project.title }}</h3>
+        <div class="d-flex mt-5">
+            <div>
+                <img class="project-img" :src="this.store.imgpath + this.project.img" alt="{{ this.project.title }}">
+            </div>
+            <div class="ms-5 mt-4">
+                <p class="fs-5 fw-bold">Description : </p>
+                <p>{{ this.project.body }}</p>
+                <p class="mb-1 fs-5 fw-bold">Technologies : </p>
+                <ul class="list-unstyled">
+                    <li class="ms-3" v-for="item in this.project.technologies" :key="item.id">{{ item.name }} <i class="{{ item.icon }}"></i></li>
+                </ul>
+            </div>
+        </div>
+    </div>
 </template>
 
 <script>
@@ -31,5 +46,9 @@ import { store } from "../store";
 </script>
 
 <style lang="scss" scoped>
+
+.project-img{
+    width: 800px;
+}
 
 </style>
